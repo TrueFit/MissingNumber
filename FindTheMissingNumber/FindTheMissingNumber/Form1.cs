010b9916b38cc06daef8cffc04d5e0f75d0dac3b
Kevin P. Hall <me@kevinsstampalbum.com>
2014-10-23 23:25:46 -0400

    Ready for review

---
+++ FindTheMissingNumber/FindTheMissingNumber/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using MissingNumberSecretSauce;
+
+namespace FindTheMissingNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var finder = new MissingNumberFinder();
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = @"txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                Title = @"Open Data Input File"
+            };
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = @"txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                Title = @"Save Output Data File"
+            };
+            if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            var inputData = finder.FetchInputData(openFileDialog.FileName);
+            var parsedData = finder.ParseInputData(inputData);
+            var resultList = finder.CalculateMissingNumbers(parsedData);
+            finder.CreateDataOutput(resultList, saveFileDialog.FileName);
+        }
+    }
+}
