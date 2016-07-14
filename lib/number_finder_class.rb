a7a2dc0d27f3b3fce743dfa93e72cacfb8dffa6b
Marc Salo <marcsalo455@gmail.com>
2016-07-14 15:22:45 -0400

    moved and renamed test.txt. Fixed output when nil

--- lib/number_finder_class.rb
+++ lib/number_finder_class.rb
@@ -5,13 +5,12 @@ class NumberFinder
 
     if(string_array.length > 1)
       unsorted_arr = to_int_array(string_array)
-
       sorted_arr = sort_numbers(unsorted_arr)
-
       missing_num = check_for_missing_number(sorted_arr)
     end
   end
 
+  private
 
   def to_string_array(input_string)
     string_array = input_string.split(",")
@@ -24,11 +23,8 @@ class NumberFinder
 
     string_array.each do |numchar|
       numchar = delete_extra_letters(numchar)
-
       number = numchar.to_i
-
       unsorted_arr[index_counter] = number
-
       index_counter = index_counter + 1
     end
 
@@ -50,7 +46,6 @@ class NumberFinder
     missing_num = nil
 
     (0..sorted_arr.length - 2).each do |k|
-
       # Checks each number agaist the next highest EXCEPT for the last number &&
       # checks for possible duplicate numbers
       if(sorted_arr[k] + 1 != sorted_arr[k + 1] && sorted_arr[k] != sorted_arr[k + 1])
