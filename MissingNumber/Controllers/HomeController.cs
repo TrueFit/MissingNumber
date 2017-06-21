7272d667b963ac93b97f058b6d7505ed79ddf56f
Fernando Leite <fevieiraleite@gmail.com>
2017-06-21 10:14:40 -0400

    Added completed project

---
+++ MissingNumber/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MissingNumber.Models;
+
+namespace MissingNumber.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult MissingNumbersCheck()
+        {
+            if (Request.Files == null || Request.Files.Count == 0)
+            {
+                return Json("NoFile");
+            }
+            else
+            { 
+                string extension = Path.GetExtension(Request.Files[0].FileName).ToLower();
+                if (extension != ".csv" && extension != ".txt")
+                {
+                    return Json("BadFileExtension");
+                }
+
+                var results = new MissingNumbersResults().getMissingNumbersFromFile(Request.Files[0]);
+
+                if (results.Input.Count == 0)
+                {
+                    return Json("BadData");
+                }
+                else
+                {
+                    return Json(results);
+                }
+            }
+        }
+    }
+}
---
+++ MissingNumber/Models/MissingNumbersResults.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MissingNumber.Models
+{
+    public class MissingNumbersResults
+    {        
+        public List<string> Input { get; set; }
+        public List<string> Output { get; set; }
+
+        
+        public MissingNumbersResults()
+        {
+            Input = new List<string>();
+            Output = new List<string>();
+        }
+
+        public MissingNumbersResults getMissingNumbersFromFile(HttpPostedFileBase file)
+        {
+            var results = new MissingNumbersResults();
+            var flatFileReader = new StreamReader(file.InputStream);
+            while (!flatFileReader.EndOfStream)
+            {
+                string line = "";
+                try
+                {
+                    line = flatFileReader.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    //Possibly log it in the future. For now, I don't care about this exception.
+                }
+                line = line.TrimStart(',').TrimEnd(',');
+
+                var numbers = new List<int>();
+                var missingNumbers = new List<int>();
+
+                if (line != null && line.Length > 0)
+                {
+                    try
+                    {
+                        numbers = line.Split(',').Select(int.Parse).ToList();
+                    }
+                    catch (Exception e)
+                    {
+                        //Possibly log it in the future. For now, I don't care about this exception.
+                    }
+
+                    if (numbers.Count > 0)
+                    {
+                        //idea here is to create a range from the minimum number of the list up until the maximum, then compare with the list we were given and get the number missing
+                        //I'm sure there are slightly more efficient algorithms, but even with 1,000,000 records it doesn't even take a second, so I consider readability more important.
+                        int differenceBetweenMinAndMax = numbers.Max() - numbers.Min();
+                        var range = Enumerable.Range(numbers.Min(), differenceBetweenMinAndMax);
+                        missingNumbers = range.Except(numbers).ToList();
+
+                        results.Input.Add(string.Join(",", numbers.Select(n => n.ToString()).ToArray())); //converts list of ints to a comma separated string
+                        results.Output.Add(string.Join(",", missingNumbers.Select(n => n.ToString()).ToArray())); //converts list of ints to a comma separated string
+                    }
+                }
+            }
+            return results;
+        }
+    }
+}
